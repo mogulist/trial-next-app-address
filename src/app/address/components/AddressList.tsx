4aebd143d5d9174958ebdac12e3408fe7d4288fd
Herbert Lim <mogulist@naver.com>
2023-11-08 01:05:49 +0900

    Responsive height

--- src/app/address/components/AddressList.tsx
+++ src/app/address/components/AddressList.tsx
@@ -11,14 +11,12 @@ type Props = {
 
 const AddressList = ({ data }: Props) => {
   return (
-    <ul className="h-60 px-4 text-sm bg-white text-black overflow-auto divide-y divide-slate-300">
+    <ul className="h-72 sm:h-96 md:h-5/6 px-4 text-sm bg-white text-black overflow-auto divide-y divide-slate-300">
       {data.map((addr) => (
-        <li key={addr.amdCd} className="flex flex-row justify-between py-1 ">
-          <div className="flex flex-col">
-            <div className="font-semibold">{addr.roadAddr}</div>
-            <div className="font-thin">{addr.jibunAddr}</div>
-          </div>
+        <li key={addr.amdCd} className="flex flex-col justify-start py-1.5 ">
           <div>{addr.zipNo}</div>
+          <div className="font-semibold">{addr.roadAddr}</div>
+          <div className="font-thin">{addr.jibunAddr}</div>
         </li>
       ))}
     </ul>
