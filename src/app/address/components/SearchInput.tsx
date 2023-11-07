727af76ba5666676569f02a5f79463d58cf14d46
Herbert Lim <mogulist@naver.com>
2023-11-07 23:17:33 +0900

    Add Search Input client component

---
+++ src/app/address/components/SearchInput.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+const SearchInput = () => {
+  const [keyword, setKeyword] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log("keyword: ", keyword);
+  }, [keyword]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit: ", keyword);
+    router.push(`/address?keyword=${keyword}`);
+  };
+
+  return (
+    <form className="w-full" onSubmit={handleSubmit}>
+      <input
+        value={keyword}
+        onChange={(event) => setKeyword(event.target.value)}
+        className="w-full py-2 px-4"
+        placeholder="주소를 입력해주세요"
+      />
+    </form>
+  );
+};
+
+export default SearchInput;
