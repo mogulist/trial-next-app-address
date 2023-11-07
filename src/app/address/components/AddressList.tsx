99158c57d69c5819d1a53d8769b2039c9a5045b4
Herbert Lim <mogulist@naver.com>
2023-11-07 23:59:51 +0900

    Show search result with sample data

---
+++ src/app/address/components/AddressList.tsx
@@ -0,0 +1,28 @@
+type Address = {
+  roadAddr: string;
+  jibunAddr: string;
+  zipNo: string;
+  amdCd: string;
+} & Record<string, string>;
+
+type Props = {
+  data: Address[];
+};
+
+const AddressList = ({ data }: Props) => {
+  return (
+    <ul className="min-h-36 max-h-72 px-4 text-sm bg-white text-black overflow-auto divide-y divide-slate-300">
+      {data.map((addr) => (
+        <li key={addr.amdCd} className="flex flex-row justify-between py-1 ">
+          <div className="flex flex-col">
+            <div className="font-semibold">{addr.roadAddr}</div>
+            <div className="font-thin">{addr.jibunAddr}</div>
+          </div>
+          <div>{addr.zipNo}</div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default AddressList;
